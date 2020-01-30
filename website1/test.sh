45b4a5b6ec2febbe336e37a1c24d7408830127d9
root <root@ip-172-31-29-30.us-west-2.compute.internal>
2020-01-30 16:38:17 +0000

    last commit for today

--- website1/test.sh
+++ website1/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get -y remove apache2
 sudo apt-get upadte
-sudo apt-get -y install apache2 
+sudo apt-get install apache2 -y
 sudo cp -r /home/ubuntu/jenkins/workspace/job1/website1/* /var/www/html
 sudo service apache2 restart
