f6a18ea618fa5252b2baad2a44a7e58f585d4d7c
root <root@ip-172-31-29-30.us-west-2.compute.internal>
2020-01-30 15:35:19 +0000

    latest commit

--- website1/test.sh
+++ website1/test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+sudo apt-get -y remove apache2
 sudo apt-get upadte
-sudo apt-get install apache2 -y
-sudo cp .* /var/www/html
+sudo apt-get -y install apache2 
+sudo cp -rf .* /var/www/html
 sudo service apache2 restart
